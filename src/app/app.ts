0dfcf991af2cd3c72296a591909da75e317e4b57
Edrei Fernandes <edreifernandes@gmail.com>
2025-08-18 15:43:18 -0300

    Using Angular Material in this project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GamesList } from './games-list/games-list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GamesList],
+  imports: [RouterOutlet, GamesList, MatSlideToggleModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
