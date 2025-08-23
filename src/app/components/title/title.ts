5747695e165fe1157c92f6099b0947e5b5e0f1b2
Edrei Fernandes <edreifernandes@gmail.com>
2025-08-23 14:47:24 -0300

    Creating Home Page

---
+++ src/app/components/title/title.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  imports: [],
+  templateUrl: './title.html',
+  styleUrl: './title.css'
+})
+export class Title {
+  
+  @Input()
+  title: String = "";
+}
