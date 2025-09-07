38397ae478dd18486c54d492ee2ac1ec8f36be93
Edrei Fernandes <edreifernandes@gmail.com>
2025-09-06 22:28:36 -0300

    filling game info using service

--- src/app/pages/game-summary/game-summary.ts
+++ src/app/pages/game-summary/game-summary.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Title } from '../../components/title/title';
 import { MatButton } from '@angular/material/button';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { GameModel } from '../../models/GameModel';
+import { GameService } from '../../sevices/game-sevice';
 
 @Component({
   selector: 'app-game-summary',
@@ -10,12 +12,13 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   styleUrl: './game-summary.css',
 })
 export class GameSummary implements OnInit {
-  id: string = "";
+  gameService = inject(GameService);
+  game: GameModel | undefined;
 
   constructor(private route: ActivatedRoute){}
   
   ngOnInit(): void {
     let idparam = this.route.snapshot.paramMap.get("gameId");
-    if(idparam) this.id = idparam;
+    if(idparam) this.game = this.gameService.getGameById(+idparam);
   }
 }
